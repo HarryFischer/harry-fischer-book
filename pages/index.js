b942058eacc28d41baafafce93e7a45c441005c4
Harry Fischer <harry.j.fischer@gmail.com>
2023-12-27 16:48:28 +0000

    add overlay

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,23 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
-import { useEffect } from 'react';
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.scss";
+import { useEffect } from "react";
 
-import image1 from '../public/images/alex-mourant.png';
-import image2 from '../public/images/brain-ops.png';
-import image3 from '../public/images/carbon-bombs.png';
-import image4 from '../public/images/eoy-2022.png';
-import image5 from '../public/images/greta.png';
-import image6 from '../public/images/mariupol.png';
-import image7 from '../public/images/cotton-capital-1.png';
-import image8 from '../public/images/cotton-capital-2.png';
-import image9 from '../public/images/food-from-4.png';
-import image10 from '../public/images/world-elephant-day.png';
+import image1 from "../public/images/alex-mourant.png";
+import image2 from "../public/images/brain-ops.png";
+import image3 from "../public/images/carbon-bombs.png";
+import image4 from "../public/images/eoy-2022.png";
+import image5 from "../public/images/greta.png";
+import image6 from "../public/images/mariupol.png";
+import image7 from "../public/images/cotton-capital-1.png";
+import image8 from "../public/images/cotton-capital-2.png";
+import image9 from "../public/images/food-from-4.png";
+import image10 from "../public/images/world-elephant-day.png";
 
 export default function Home() {
   useEffect(() => {
     const pannerCode = () => {
-      const panTag = document.getElementById('panner-inner');
+      const panTag = document.getElementById("panner-inner");
       let currentX = 0;
       let currentY = 0;
       let aimX = 0;
@@ -45,32 +45,66 @@ export default function Home() {
       const animate = function () {
         currentX += (aimX - currentX) * 0.05;
         currentY += (aimY - currentY) * 0.05;
-        panTag.style.left = -1 * currentX + 'px';
-        panTag.style.top = -1 * currentY + 'px';
+        panTag.style.left = -1 * currentX + "px";
+        panTag.style.top = -1 * currentY + "px";
         requestAnimationFrame(animate);
       };
       animate();
-      document.addEventListener('mousemove', function (event) {
+      document.addEventListener("mousemove", function (event) {
         updateAim(event);
       });
-      document.addEventListener('touchmove', function (event) {
+      document.addEventListener("touchmove", function (event) {
         updateAim(event);
+      });
+    };
+
+    const lightBox = () => {
+      const allImages = document.querySelectorAll("img");
+      const lightBoxWrapper = document.getElementById("lightbox-wrapper");
+      allImages.forEach((image) => {
+        console.log(image.src);
+        image.addEventListener("click", () => {
+          const lightBoxInner = document.querySelector(".lightbox-inner");
+          const lightBoxInnerImage = lightBoxInner.querySelector("img");
+          lightBoxInnerImage.src = image.src;
+          console.log(lightBoxInner);
+          console.log("working");
+
+          const closeButton = lightBoxInner.querySelector(
+            ".lightbox-inner__close"
+          );
+          lightBoxWrapper.classList.add("show");
+          lightBoxWrapper.classList.add(styles["show"]);
+          closeButton.addEventListener("click", () => {
+            lightBoxWrapper.classList.remove("show");
+            lightBoxWrapper.classList.remove(styles["show"]);
+          });
+        });
+      });
+    };
+
+    const showOverlay = () => {
+      const overlay = document.getElementById("overlay");
+      overlay.addEventListener("click", () => {
+        overlay.classList.add("show");
       });
     };
 
     const clickToEnter = () => {
-      const clickArea = document.getElementById('click-me');
-      clickArea.addEventListener('click', () => {
-        clickArea.classList.add('clicked');
-        clickArea.style.opacity = '0';
+      const clickArea = document.getElementById("click-me");
+      clickArea.addEventListener("click", () => {
+        clickArea.classList.add("clicked");
+        clickArea.style.opacity = "0";
         setTimeout(() => {
-          clickArea.style.display = 'none';
+          clickArea.style.display = "none";
         }, 1000);
       });
     };
 
-    pannerCode();
     clickToEnter();
+    pannerCode();
+    showOverlay();
+    lightBox();
   }, []);
 
   return (
@@ -78,127 +112,65 @@ export default function Home() {
       <Head>
         <title>Harry Fischer — Digital Design Lead</title>
         <meta
-          name='description'
-          content='Harry Fischer — Digital Design Lead'
+          name="description"
+          content="Harry Fischer — Digital Design Lead"
         />
-        <link rel='icon' href='/favicon.ico' />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <section className={styles.bigh} id='click-me'>
+      <section className={styles.bigh} id="click-me">
         <div className={styles.top}></div>
         <div className={styles.bottom}></div>
       </section>
 
-      <main className={styles.panner} id='panner'>
+      <div className={styles.lightbox} id="lightbox-wrapper">
+        <div className="lightbox-inner">
+          <button className="lightbox-inner__close">Close</button>
+          <img src="" alt="" />
+        </div>
+      </div>
+
+      <main className={styles.panner} id="panner">
         <button>Information</button>
-        <div className={styles.inner} id='panner-inner'>
+        <div className={styles.inner} id="panner-inner">
           <div className={styles.item}>
-            <details>
-              <summary>
-                <h2>Project title</h2>
-              </summary>
-              Hello hello hello hello hello hello hello hello hello hello hello
-            </details>
-            <Image alt='/' src={image1} />
+            <div className={styles.overlay} id="overlay">
+              <h3> Alexander Mourant portfolio</h3>
+            </div>
+            <Image alt="/" src={image1} />
           </div>
           <div className={styles.item}>
-            <details>
-              <summary>
-                <h2>Project title</h2>
-              </summary>
-              Hello hello hello hello hello hello hello hello hello hello hello
-            </details>
-            <Image alt='/' src={image2} />
+            <Image alt="/" src={image2} />
           </div>
           <div className={styles.item}>
-            <details>
-              <summary>
-                <h2>Project title</h2>
-              </summary>
-              Hello hello hello hello hello hello hello hello hello hello hello
-            </details>
-            <Image alt='/' src={image3} />
+            <Image alt="/" src={image3} />
           </div>
           <div className={styles.item}>
-            <details>
-              <summary>
-                <h2>Project title</h2>
-              </summary>
-              Hello hello hello hello hello hello hello hello hello hello hello
-            </details>
-            <Image alt='/' src={image4} />
+            <Image alt="/" src={image4} />
           </div>
           <div className={styles.item}>
-            <details>
-              <summary>
-                <h2>Project title</h2>
-              </summary>
-              Hello hello hello hello hello hello hello hello hello hello hello
-            </details>
-            <Image alt='/' src={image5} />
+            <Image alt="/" src={image5} />
           </div>
           <div className={styles.item}>
-            <details>
-              <summary>
-                <h2>Project title</h2>
-              </summary>
-              Hello hello hello hello hello hello hello hello hello hello hello
-            </details>
-            <Image alt='/' src={image6} />
+            <Image alt="/" src={image6} />
           </div>
           <div className={styles.item}>
-            <details>
-              <summary>
-                <h2>Project title</h2>
-              </summary>
-              Hello hello hello hello hello hello hello hello hello hello hello
-            </details>
-            <Image alt='/' src={image7} />
+            <Image alt="/" src={image7} />
           </div>
           <div className={styles.item}>
-            <details>
-              <summary>
-                <h2>Project title</h2>
-              </summary>
-              Hello hello hello hello hello hello hello hello hello hello hello
-            </details>
-            <Image alt='/' src={image8} />
+            <Image alt="/" src={image8} />
           </div>
           <div className={styles.item}>
-            <details>
-              <summary>
-                <h2>Project title</h2>
-              </summary>
-              Hello hello hello hello hello hello hello hello hello hello hello
-            </details>
-            <Image alt='/' src={image9} />
+            <Image alt="/" src={image9} />
           </div>
           <div className={styles.item}>
-            <details>
-              <summary>
-                <h2>Project title</h2>
-              </summary>
-              Hello hello hello hello hello hello hello hello hello hello hello
-            </details>
-            <Image alt='/' src={image10} />
+            <Image alt="/" src={image10} />
           </div>
           <div className={styles.item}>
-            <details>
-              <summary>
-                <h2>Project title</h2>
-              </summary>
-              Hello hello hello hello hello hello hello hello hello hello hello
-            </details>
-            <Image alt='/' src={image3} />
+            <Image alt="/" src={image3} />
           </div>
           <div className={styles.item}>
-            <details>
-              <summary>
-                <h2>Project title</h2>
-              </summary>
-              Hello hello hello hello hello hello hello hello hello hello hello
-            </details>
-            <Image alt='/' src={image4} />
+            <Image alt="/" src={image4} />
           </div>
         </div>
       </main>
